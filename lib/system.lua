769c6c9d1c5187ce6ace5bb856afcc1ba9732176
Arnaught <rayquaza01@outlook.com>
2024-08-23 10:11:10 -0400

    Add window functions, move window() from system to windows

--- lib/system.lua
+++ lib/system.lua
@@ -11,24 +11,6 @@
 --- @field window_attribs __WindowAttribs
 local __Environment = {}
 
---- Window Attributes
---- @class __WindowAttribs
---- @field fullscreen? boolean
---- @field has_frame? boolean
---- @field height? integer
---- @field moveable? boolean
---- @field resizable? boolean
---- @field show_in_workspace? boolean
---- @field tabbed? boolean
---- @field title? string
---- @field video_mode? 0 | 3 | 4
---- @field wallpaper? boolean
---- @field width? integer
---- @field x? integer
---- @field y? integer
---- @field z? integer
-local __WindowAttribs = {}
-
 --- Gets the environment variables given to the process at its creation
 --- [View Online](https://www.lexaloffle.com/dl/docs/picotron_manual.html#env)
 --- @return __Environment
@@ -37,14 +19,6 @@ function env() end
 --- Exits the program
 --- @param exit_code integer
 function exit(exit_code) end
-
---- @param width integer
---- @param height integer
---- @param attribs? __WindowAttribs
-function window(width, height, attribs) end
-
---- @param attribs __WindowAttribs
-function window(attribs) end
 
 --- Prints text to the host system's console
 --- [View Online](https://www.lexaloffle.com/dl/docs/picotron_manual.html#printh)
