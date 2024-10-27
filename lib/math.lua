99ae80424f55bf5684ee6fc5b5ce6bea91f62445
Arnaught <rayquaza01@outlook.com>
2024-10-27 00:55:23 -0400

    Start math

---
+++ lib/math.lua
@@ -0,0 +1,29 @@
+--- @meta
+
+--- Returns the minimum of two numbers
+--- [View Online](https://www.lexaloffle.com/dl/docs/pico-8_manual.html#MIN)
+--- @param x number
+--- @param y number
+--- @return number
+function min(x, y) end
+
+--- Returns the maximum of two numbers
+--- [View Online](https://www.lexaloffle.com/dl/docs/pico-8_manual.html#MAX)
+--- @param x number
+--- @param y number
+--- @return number
+function max(x, y) end
+
+--- Returns the middle value of two numbers
+--- [View Online](https://www.lexaloffle.com/dl/docs/pico-8_manual.html#MID)
+--- @param x number
+--- @param y number
+--- @param z number
+--- @return number
+function mid(x, y, z) end
+
+--- Floor a number
+--- [View Online](https://www.lexaloffle.com/dl/docs/pico-8_manual.html#FLR)
+--- @param x number
+--- @return integer
+function flr(x) end
