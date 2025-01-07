d7c08d77156d41bd3379782cb7728ec3e7c21aa5
Arnaught <rayquaza01@outlook.com>
2025-01-07 17:52:16 -0500

    [gui] Add wrangle working file

--- lib/gui.lua
+++ lib/gui.lua
@@ -58,3 +58,22 @@ function __GUI_ED:set_cursor(column, line) end
 --- @return string[]
 --- Set the text editor's current text
 function __GUI_ED:get_text() end
+
+--- Manage working with a file
+--- save_state is a function that returns the content and metadata of the file to save. Called when the "save_file" event is fired
+--- load_state is a function that takes the content and metadata of the file as parameters. Called when the "open_file" event is fired
+--- untitled filename is the file name to use by default
+--- get_hlocation is a function that gets the location in the file
+--- set_hlocation is a function that sets the location in the file
+--- View implementation in /system/lib/wrangle.lua
+--- @param save_state? function
+--- @param load_state? function
+--- @param untitled_filename? string
+--- @param get_hlocation? function
+--- @param set_hlocation? function
+function wrangle_working_file(save_state, load_state, untitled_filename, get_hlocation, set_hlocation) end
+
+--- Gets the present working file. Used with wrangle_working_file
+--- View implementation in /system/lib/wrangle.lua
+--- @return string | nil
+function pwf() end
